3660dfc38b70edf1668ca564093c1fd464bcff1d
Sanyam Jain <xerosanyam@gmail.com>
2015-12-23 12:01:18 +0530

    Images proper.

--- app/src/main/java/com/example/sanyam/popmovies/MainActivity.java
+++ app/src/main/java/com/example/sanyam/popmovies/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sanyam.popmovies;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -10,6 +11,9 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     GridView gridView;
@@ -30,45 +34,64 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public class ImageAdapter extends BaseAdapter {
-        private Context mcontext;
-        private Integer[] mThumbsIds = {
-                R.drawable.sample_0,
-                R.drawable.sample_1,
-                R.drawable.sample_2,
-                R.drawable.sample_3,
-                R.drawable.sample_4,
-                R.drawable.sample_5,
-                R.drawable.sample_6,
-                R.drawable.sample_7
-        };
+    private final class ImageAdapter extends BaseAdapter {
+        private final List<Item> mItems = new ArrayList<Item>();
+        private final LayoutInflater mInflater;
 
-        public ImageAdapter(Context c) {
-            mcontext = c;
+        public ImageAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+
+            mItems.add(new Item("Red", R.drawable.sample_0));
+            mItems.add(new Item("Magenta", R.drawable.sample_1));
+            mItems.add(new Item("Dark Gray", R.drawable.sample_2));
+            mItems.add(new Item("Gray", R.drawable.sample_3));
+            mItems.add(new Item("Green", R.drawable.sample_4));
+            mItems.add(new Item("Cyan", R.drawable.sample_5));
+            mItems.add(new Item("Cyan", R.drawable.sample_6));
+            mItems.add(new Item("Cyan", R.drawable.sample_7));
         }
 
+        @Override
         public int getCount() {
-            return mThumbsIds.length;
+            return mItems.size();
         }
 
-        public Object getItem(int position) {
-            return null;
+        @Override
+        public Item getItem(int position) {
+            return mItems.get(position);
         }
 
+        @Override
         public long getItemId(int position) {
-            return 0;
+            return mItems.get(position).drawableId;
         }
 
+        @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
+            ImageView picture;
+
             if (convertView == null) {
-                imageView = new ImageView(mcontext);
-                //imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            } else {
-                imageView = (ImageView) convertView;
+                convertView = mInflater.inflate(R.layout.grid_item, parent, false);
+                convertView.setTag(R.id.picture, convertView.findViewById(R.id.picture));
             }
-            imageView.setImageResource(mThumbsIds[position]);
-            return imageView;
+
+            picture = (ImageView) convertView.getTag(R.id.picture);
+
+            Item item = getItem(position);
+
+            picture.setImageResource(item.drawableId);
+
+            return convertView;
+        }
+
+        private class Item {
+            public final String name;
+            public final int drawableId;
+
+            Item(String name, int drawableId) {
+                this.name = name;
+                this.drawableId = drawableId;
+            }
         }
     }
 }
